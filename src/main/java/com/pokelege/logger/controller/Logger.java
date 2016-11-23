35aa4df0db749a85a89a41bf61562b2a2a756d4f
Andrew <andrew@lootinteractive.com>
2016-11-22 19:20:26 -0800

    fix

--- src/main/java/com/pokelege/logger/controller/Logger.java
+++ src/main/java/com/pokelege/logger/controller/Logger.java
@@ -55,7 +55,7 @@ public class Logger {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 
-		Query query = session.createQuery("select * from LogString");
+		Query query = session.createQuery("from LogString");
 		List<LogString> list = query.list();
 		session.close();
 		ModelAndView mv = new ModelAndView("logstring");
