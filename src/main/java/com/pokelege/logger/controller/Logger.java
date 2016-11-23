70aaa2958a3df8a969c9c9bb418a962702893ebd
Andrew <andrew@lootinteractive.com>
2016-11-22 19:16:47 -0800

    some tests

--- src/main/java/com/pokelege/logger/controller/Logger.java
+++ src/main/java/com/pokelege/logger/controller/Logger.java
@@ -4,6 +4,7 @@ import com.pokelege.logger.entity.LogString;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import sun.rmi.runtime.Log;
+
+import java.util.List;
 
 @Controller
 public class Logger {
@@ -42,5 +46,20 @@ public class Logger {
 		session.getTransaction().commit();
 		session.close();
 		return new ResponseEntity<String>(HttpStatus.OK);
+	}
+
+	@RequestMapping("/logString")
+	public ModelAndView logString(){
+		SessionFactory sessionFactory = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		Query query = session.createQuery("select * from LogString");
+		List<LogString> list = query.list();
+		session.close();
+		ModelAndView mv = new ModelAndView("logstring");
+		mv.addObject("messages", list);
+		return mv;
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/logstring.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Logstring</title>
+</head>
+<body>
+	<center>
+		<h2>Logs</h2>
+		<table>
+		<tr>
+		<th>Instance ID</th>
+		<th>Message</th>
+		</tr>
+        <% for(int i = 0; i < messages.size(); i+=1) { %>
+                <tr>
+                <td><%=messages.get(i).getInstanceId()%></td>
+                    <td><%=messages.get(i).getMessage()%></td>
+                </tr>
+            <% } %>
+		</table>
+	</center>
+</body>
+</html>
